e2f4810707255e07ccaf95ce33ff2d054e40f026
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-23 21:38:05 +0530

    Mix & Match types

--- Intro_To_TS/TypeAliases.ts
+++ Intro_To_TS/TypeAliases.ts
@@ -11,3 +11,43 @@ function createUser(user: User){
 }
 
 createUser({name: '', email: '', isActive: true})
+
+// 
+
+type UserType = {
+    readonly _id: string,
+    name: string,
+    email: string,
+    isActive: boolean,
+    creditCardDetails?: number, // '?' denotes optional field
+}
+
+let myUser: UserType = {
+    _id: '123400',
+    name: 'Sdas',
+    email: 'sdas@gmail.com',
+    isActive: true,
+}
+
+myUser.email = 'new email'
+// myUser._id = 'new id' // Will throw error as _id is marked as readonly
+
+// Mix and Match of types
+
+type cardNumber = {
+    cardnumber: string,
+}
+
+type cardDate = {
+    carddate: string,
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number,
+}
+
+let card: cardDetails = {
+    cardnumber: '1234',
+    carddate: '23/09/24',
+    cvv: 123
+}
