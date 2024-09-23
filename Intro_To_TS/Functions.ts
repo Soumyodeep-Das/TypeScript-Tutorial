81cbf8293aa6477f1d3cccf09bfaf3426c01cea1
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-09-23 21:09:22 +0530

    Output type check in TS

--- Intro_To_TS/Functions.ts
+++ Intro_To_TS/Functions.ts
@@ -14,3 +14,34 @@ function signUpUser(name : string, username: string, isAdult: boolean){
 
 addTwo(5)
 getUpper("a")
+
+// Functions with both input and output type check
+function addThree(num : number) : number {
+    // return 'anything' // will throw error
+    return num+3;
+}
+
+addThree(9);
+
+const getHello = (s: string) : string => {
+    return `Hello ${s}`;
+}
+
+getHello('SDas');
+
+const heros = ["Thor", "Spidy", "IronMan"]
+
+heros.map((hero: string) => {
+    return `Hero is ${hero}`;
+})
+
+// Functions where we aren't returning any value
+function consoleError(errMsg: string): void{
+    console.log(errMsg);
+    // return 'something' // will throw error
+}
+
+// Functions that never returns a value
+function handleError(errMsg: string): never{
+    throw new Error(errMsg)
+}
